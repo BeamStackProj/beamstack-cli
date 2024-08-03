73398a6958a45b76afd583d733e6f99654445bb7
Nathan-Salawe <nate@mavencode.com>
2024-08-03 01:19:20 +0100

    create beamjob

--- src/cmd/deploy/pipeline.go
+++ src/cmd/deploy/pipeline.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"path/filepath"
 
@@ -15,6 +16,7 @@ import (
 	"github.com/BeamStackProj/beamstack-cli/src/utils"
 	"github.com/spf13/cobra"
 	"gopkg.in/yaml.v2"
+	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -23,6 +25,8 @@ import (
 var (
 	flinkCluster string = ""
 	PVCMountPath string = "/pvc"
+	JobName      string = "beamjob"
+	Parallelism  uint8  = 1
 )
 
 type FileInfo struct {
@@ -45,8 +49,20 @@ func init() {
 }
 
 func DeployPipeline(cmd *cobra.Command, args []string) {
+	profile, err := utils.ValidateCluster()
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if profile.Operators.Flink == nil {
+		fmt.Println("Flink Operator not initialized on this cluster")
+		return
+	}
+	flinkVersion := strings.Replace(profile.Operators.Flink.Version, ".", "_", 1)
+
 	pipeline := &types.Pipeline{}
-	err := utils.ParseYAML(args[0], pipeline)
+	err = utils.ParseYAML(args[0], pipeline)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -206,13 +222,65 @@ func DeployPipeline(cmd *cobra.Command, args []string) {
 		fmt.Println(err)
 		return
 	}
+	now := time.Now()
+	dateTime := now.Format("2006-01-02-150405")
+	jobName := fmt.Sprintf("%s-%s", "pipeline", dateTime)
+	_, err = objects.CreateJob(
+		clientset,
+		batchv1.Job{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      jobName,
+				Namespace: "flink",
+			},
+			Spec: batchv1.JobSpec{
+				Template: v1.PodTemplateSpec{
+					ObjectMeta: metav1.ObjectMeta{
+						Labels: map[string]string{"app": jobName},
+					},
+					Spec: v1.PodSpec{
+						Containers: []v1.Container{
+							{
+								Name:    "beam-pipeline",
+								Image:   fmt.Sprintf("beamstackproj/flink-harness%s:latest", flinkVersion),
+								Command: []string{"python"},
+								Args: []string{
+									"-m", "apache_beam.yaml.main", fmt.Sprintf("--pipeline_spec_file=%s", filepath.Join(PVCMountPath, filename)),
+									"--runner=FlinkRunner", fmt.Sprintf("--flink_master=%s-rest.flink.svc.cluster.local:8081", flinkCluster),
+									fmt.Sprintf("--job_name=%s", JobName), fmt.Sprintf("--parallelism=%s", string(Parallelism)), "--environment_type=EXTERNAL",
+									"--environment_config=localhost:50000", "--flink_submit_uber_jar",
+								},
+								VolumeMounts: []v1.VolumeMount{
+									{
+										Name:      "migration-volume",
+										MountPath: PVCMountPath,
+									},
+								},
+							},
+						},
+						Volumes: []v1.Volume{
+							{
+								Name: "migration-volume",
+								VolumeSource: v1.VolumeSource{
+									PersistentVolumeClaim: &v1.PersistentVolumeClaimVolumeSource{
+										ClaimName: fmt.Sprintf("%s-pvs", flinkCluster),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	)
 
-	// TODO
-	// move pipeline yaml file to pvc
-	// create job
-	// monitory job
-	// move file from pvc to local system
-	// delete migration pod
+	if err != nil {
+		fmt.Printf("could not create pipeline job %s\n", err)
+	}
+
+	err = os.Remove(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
 
 func SavePipeline(data interface{}) (string, error) {
